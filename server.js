e4f996f42b8a5526c6047323bc181d4b3de6d96c
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-01-10 00:17:00 +0530

    9/1/2026/6

--- server.js
+++ server.js
@@ -222,10 +222,9 @@ app.prepare().then(() => {
         });
     });
 
-    const PORT = process.env.PORT || 3000;
-    server.listen(PORT, (err) => {
+    server.listen(3002, (err) => {
         if (err) throw err;
-        console.log(`> Server Ready on http://localhost:${PORT} (Ed25519 Mode)`);
+        console.log('> Server Ready on http://localhost:3002 (Ed25519 Mode)');
 
         // TTL Cleaner (Every 60s)
         setInterval(async () => {
